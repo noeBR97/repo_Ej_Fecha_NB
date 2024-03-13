32a5c026a7f6962285f91e8c019b1db89c5e3b35
Noelia Barrionuevo <noe.br97@gmail.com>
2024-03-13 10:54:14 +0100

    cambios en la función valida. Fin refactorización

--- Ej_Fecha_NB/src/main/kotlin/Fecha.kt
+++ Ej_Fecha_NB/src/main/kotlin/Fecha.kt
@@ -4,13 +4,12 @@ class Fecha(private val dia: Int, private val mes: Int, private val anio: Int) {
 
 
     fun valida(): Boolean {
-
-        if (dia < 1 || dia > 31) return false
-        if (mes < 1 || mes > 12) return false
-
-        // determinamos la cantidad de días del mes:
-        val diaMes = diadelMes(mes)
-        return diaMes
+        var esValida=false
+        if ((dia < 1 || dia > 31) && (mes < 1 || mes > 12)){
+            var diaMes = diadelMes(mes)
+            esValida = dia<=diaMes
+        }
+        return esValida
     } // … más métodos
 
     private fun diadelMes(mes: Int): Int {
