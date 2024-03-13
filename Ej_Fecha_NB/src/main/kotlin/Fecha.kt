82962c4698f93376575ee3ac462c7712df5ff0d2
Noelia Barrionuevo <noe.br97@gmail.com>
2024-03-13 11:34:58 +0100

    Revert "Refactorización - cambio nombre método y variables"
    This reverts commit df4e46605921f93f2c1e1d2c4f4201dba71ac047.

--- Ej_Fecha_NB/src/main/kotlin/Fecha.kt
+++ Ej_Fecha_NB/src/main/kotlin/Fecha.kt
@@ -1,25 +1,25 @@
 package org.example
 
-class Fecha(private val dia: Int, private val mes: Int, private val anio: Int) {
+class Fecha(private val d: Int, private val m: Int, private val a: Int) {
 
 
     fun valida(): Boolean {
-        var esValida=false
-        if ((dia < 1 || dia > 31) && (mes < 1 || mes > 12)){
-            var diaMes = diadelMes(mes)
-            esValida = dia<=diaMes
-        }
-        return esValida
+
+        if (d < 1 || d > 31) return false
+        if (m < 1 || m > 12) return false
+
+        // determinamos la cantidad de días del mes:
+        return diaMes()
     } // … más métodos
 
-    private fun diadelMes(mes: Int): Int {
+    private fun diaMes(): Boolean {
         var diasMes = 0
-        when (mes) {
+        when (m) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
             2 -> diasMes = if (esBisiesto(anio)) 29 else 28
         }
-        return diasMes
+        return if (d > diasMes) false else true
     }
 
     private fun esBisiesto(anio: Int) = anio % 400 == 0 || anio % 4 == 0 && anio % 100 != 0
