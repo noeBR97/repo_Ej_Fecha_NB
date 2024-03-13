ca3adfd8e4c741787fa8f725d391fc4d1c543077
Noelia Barrionuevo <noe.br97@gmail.com>
2024-03-13 10:49:20 +0100

    varios cambios en variables y parámetros

--- Ej_Fecha_NB/src/main/kotlin/Fecha.kt
+++ Ej_Fecha_NB/src/main/kotlin/Fecha.kt
@@ -9,19 +9,19 @@ class Fecha(private val dia: Int, private val mes: Int, private val anio: Int) {
         if (mes < 1 || mes > 12) return false
 
         // determinamos la cantidad de días del mes:
-        val diaMes = diadelMes()
+        val diaMes = diadelMes(mes)
         return diaMes
     } // … más métodos
 
-    private fun diadelMes(): Boolean {
+    private fun diadelMes(mes: Int): Int {
         var diasMes = 0
         when (mes) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (esBisiesto()) 29 else 28
+            2 -> diasMes = if (esBisiesto(anio)) 29 else 28
         }
-        return if (dia > diasMes) false else true
+        return diasMes
     }
 
-    private fun esBisiesto() = anio % 400 == 0 || anio % 4 == 0 && anio % 100 != 0
+    private fun esBisiesto(anio: Int) = anio % 400 == 0 || anio % 4 == 0 && anio % 100 != 0
 }
